a9534f55db4f13325031a11226c2c23b36efe0d2
CodeByLine <yleventhal@ymail.com>
2016-08-19 14:25:22 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    @author = @post.author
+    @author = @post.author #not needed anymore w locals
   end
 
   def index
