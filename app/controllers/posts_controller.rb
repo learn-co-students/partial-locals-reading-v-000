990490d6b1807e7572c8e46e4b80bf19f89e5ad2
alexcrisan <alexcrisan2005@hotmail.com>
2018-06-07 06:57:27 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    @author = @post.author
+    # @author = @post.author -- this line is no longer needed because we are using a partial
   end
+
 
   def index
     @posts = Post.all
